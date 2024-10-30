269ef4dabdf7948badd0871ead834d1423b19b44
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2024-10-30 18:05:50 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -2,10 +2,62 @@
 using namespace std;
 
 int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    float n, max, min, somma, positivi, negativi, i;
+    positivi =0;
+    somma =0;
+    i=0;
+    negativi=0;
+    do
+    {
+        cout << "inserisci numero" << endl;
+        cin >>n;
+        if (i=0)
+        {
+            max = n;
+        }
+        else
+        {
+            if (n>max)
+            {
+                max=n;
+            }
+            else
+            {
+
+            }
+        }
+        if (i=0)
+        {
+            min = n;
+        }
+        else
+        {
+            if (n<min)
+            {
+                min=n;
+            }
+            else
+            {
+
+            }
+        }
+        if (n>0)
+        {
+          somma = somma +n;
+          positivi = positivi +1;
+        }
+        else
+        {
+            negativi = negativi +1;
+        }
+        i = i +1;
+
+    } while (n!=0);
+    cout << "il numero piu grande è " << max << endl;
+    cout << "il numero piu piccolo è " << min << endl;
+    cout << "sono stati inseriti " << negativi << " numeri negativi" << endl;
+    cout << "la media di tutti i valori positivi è " << somma/positivi << endl;
+    
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
